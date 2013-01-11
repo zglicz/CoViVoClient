d8713deda79bc9e7f4c438c940b9f6333759bd2c
Tomasz Wiatrowski <daredeva@gmail.com>
2013-01-12 00:28:13 +0100

    dodany CovUdpClient

---
+++ CoViVoClient/CovUdpClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using WrapperLib;
+
+namespace CoViVoClient
+{
+    class CovUdpClient
+    {
+        private const int udp_port = 9051;
+        private const string host = "localhost";
+        private UdpClient udp_client;
+        private string nick;
+
+        public CovUdpClient(String nick) {
+            this.udp_client = new UdpClient(udp_port+1);
+            this.nick = nick;
+        }
+
+        public bool connect() {
+            try {
+                udp_client.Connect(host, udp_port);
+            }
+            catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+
+        public void sendMessage(String msg, String channel) {
+            Text to_send = new Text();
+            to_send.user = nick;
+            to_send.text = msg;
+            to_send.channelName = channel;
+            byte[] wrappedMessage = Util.Wrap(to_send);
+            udp_client.Send(wrappedMessage, wrappedMessage.Length);
+        }
+
+        public void proveAlive() {
+            Alive alive = new Alive();
+            alive.user = nick;
+            byte[] wrappedMessage = Util.Wrap(alive);
+            udp_client.Send(wrappedMessage, wrappedMessage.Length);
+        }
+
+        public void proover() {
+            while (true) {
+                proveAlive();
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        public void listen() {
+            while (true) {
+                IPAddress addr = IPAddress.Any;
+                IPEndPoint ipep = new IPEndPoint(addr, 0);
+                byte[] messageWrapped = udp_client.Receive(ref ipep);
+                Message messageUnwrapped = WrapperLib.Util.Unwrap(messageWrapped);
+                Console.WriteLine(messageUnwrapped);
+            }
+        }
+
+        public void keybordRead() {
+            while (true) {
+                String message = Console.ReadLine();
+                if (message != null) {
+                    sendMessage(message, "sampleChannel");
+                }
+            }
+        }
+    }
+}
